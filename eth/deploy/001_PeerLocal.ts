ac6c157e8f2bacfebdfc8972282bb32b83718036
grmkris <kristjan.grm1@gmail.com>
2023-06-09 15:05:37 +0200

    update stuff

--- eth/deploy/001_PeerLocal.ts
+++ eth/deploy/001_PeerLocal.ts
@@ -9,15 +9,31 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await getNamedAccounts();
   if (!deployer) throw new Error("deployer is undefined");
 
-  const helloWorld = await deploy("PeerLocal", {
-    from: deployer,
-    log: true,
-    // GHO on sepolia https://docs-gho.vercel.app/developer-docs/contracts-overview#sepolia-gho-deployment:~:text=0x894ef447CF3C97F267999244B1D130Bd746153E6-,GhoToken,0x5d00fab5f2F97C4D682C1053cDCAA59c2c37900D,-GhoOracle
-    args: ["0x5d00fab5f2F97C4D682C1053cDCAA59c2c37900D"],
-    autoMine: true,
-  });
+  const gho_sepolia = "0x5d00fab5f2F97C4D682C1053cDCAA59c2c37900D";
+  const gho_goerli = "0xcbE9771eD31e761b744D3cB9eF78A1f32DD99211";
 
-  console.log("PeerLocal deployed to:", helloWorld.address);
+  switch (hre.network.name) {
+    case "sepolia":
+      const deployment1 = await deploy("PeerLocal", {
+        from: deployer,
+        log: true,
+        // GHO on sepolia https://docs-gho.vercel.app/developer-docs/contracts-overview#sepolia-gho-deployment:~:text=0x894ef447CF3C97F267999244B1D130Bd746153E6-,GhoToken,0x5d00fab5f2F97C4D682C1053cDCAA59c2c37900D,-GhoOracle
+        args: [gho_sepolia],
+        autoMine: true,
+      });
+      console.log("PeerLocal deployed to:", deployment1.address);
+      break;
+    case "goerli":
+      const deployment2 = await deploy("PeerLocal", {
+        from: deployer,
+        log: true,
+        // GHO on sepolia https://docs-gho.vercel.app/developer-docs/contracts-overview#sepolia-gho-deployment:~:text=0x894ef447CF3C97F267999244B1D130Bd746153E6-,GhoToken,0x5d00fab5f2F97C4D682C1053cDCAA59c2c37900D,-GhoOracle
+        args: [gho_goerli],
+        autoMine: true,
+      });
+      console.log("PeerLocal deployed to:", deployment2.address);
+      break;
+  }
 };
 
 export default func;
