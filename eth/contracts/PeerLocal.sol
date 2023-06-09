f1bb821070ac64870d0a0255b115f79cd36bd2fd
grmkris <kristjan.grm1@gmail.com>
2023-06-09 12:46:18 +0200

    added events

--- eth/contracts/PeerLocal.sol
+++ eth/contracts/PeerLocal.sol
@@ -23,6 +23,13 @@ contract PeerLocal is Ownable {
         uint256 stakingRequirement;
     }
 
+    event CommunityCreated(uint256 indexed communityId, string ipfsMetadata, uint256 stakingRequirement, address indexed owner);
+    event MemberJoinedCommunity(uint256 indexed communityId, address indexed member);
+    event OfferCreated(uint256 indexed communityId, uint256 offerId, address indexed owner, string metadata, uint256 reputationRequirement, uint256 stakingRequirement);
+    event OfferAccepted(uint256 indexed communityId, uint256 indexed offerId, address indexed member);
+
+
+
     IERC20 public token; // GHO
     mapping(uint256 => Community) public communities;
     uint256 communityCounter = 0;
@@ -40,6 +47,7 @@ contract PeerLocal is Ownable {
     function createCommunity(string memory ipfsMetadata, uint256 stakingRequirement) public {
         communities[communityCounter] = Community({ipfsMetadata: ipfsMetadata, stakingRequirement: stakingRequirement, owner: msg.sender});
         communityCounter++;
+        emit CommunityCreated(communityCounter - 1, ipfsMetadata, stakingRequirement, msg.sender);
     }
 
     function joinCommunity(uint256 communityId, bytes memory signature) public {
@@ -51,11 +59,16 @@ contract PeerLocal is Ownable {
         token.transferFrom(msg.sender, address(this), communities[communityId].stakingRequirement);
         // add msg.sender to communityMembers
         communityMembers[communityId].push(msg.sender);
+        // add reputation to msg.sender
+        reputation[msg.sender] += 1;
+        // emit event
+        emit MemberJoinedCommunity(communityId, msg.sender);
     }
 
     function createOffer(uint256 communityId, string memory metadata, uint256 reputationRequirement, uint256 stakingRequirement) public {
         require(reputation[msg.sender] >= reputationRequirement, "Insufficient reputation to create offer");
         offers[communityId].push(Offer({owner: msg.sender, communityId: communityId, metadata: metadata, reputationRequirement: reputationRequirement, stakingRequirement: stakingRequirement}));
+        emit OfferCreated(communityId, offers[communityId].length - 1, msg.sender, metadata, reputationRequirement, stakingRequirement);
     }
 
     function acceptOffer(uint256 communityId, uint256 offerId) public {
@@ -63,6 +76,10 @@ contract PeerLocal is Ownable {
         require(reputation[msg.sender] >= offers[communityId][offerId].reputationRequirement, "Insufficient reputation to accept offer");
         // Transfer staked tokens
         token.transferFrom(msg.sender, address(this), offers[communityId][offerId].stakingRequirement);
+        // Transfer staked tokens to offer owner
+        token.transfer(offers[communityId][offerId].owner, offers[communityId][offerId].stakingRequirement);
+        // emit event
+        emit OfferAccepted(communityId, offerId, msg.sender);
     }
 
     function _recoverSigner(
